5536c810e6dd6f5e2379a5b5a6483e34b80bbce4
murray0809 <tomo19940809@gmail.com>
2019-11-21 22:52:58 +0900

    add asset

---
+++ 第3ターム　審査会/Assets/BreakableBlockController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attack タグを持つオブジェクトのトリガーとの接触が発生したら、パーティクルを再生してオブジェクトを消す機能を提供するコンポーネント
+/// </summary>
+[RequireComponent(typeof(ParticleSystem))]
+public class BreakableBlockController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Attack")
+        {
+            // ブロックのコライダーを無効にする
+            Collider col = GetComponent<Collider>();
+            col.enabled = false;
+
+            // パーティクルを再生する
+            ParticleSystem ps = GetComponent<ParticleSystem>();
+            ps.Play();
+            Destroy(this.gameObject, ps.main.duration); // 再生が終わったら破棄する
+
+            // 描画を消す
+            Renderer r = GetComponent<Renderer>();
+            r.enabled = false;
+        }
+    }
+}
